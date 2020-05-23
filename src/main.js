f7623f7a0dc1f6f4c5c06313fa13204ce0d6d13d
kvoncode <kvoncode@gmail.com>
2020-05-23 14:43:16 +0900

    add: vuex

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from "vue";
+import Vuex from "vuex";
 import App from "./App.vue";
 
 import "materialize-css/dist/css/materialize.css";
 import "normalize.css";
+
+Vue.use(Vuex);
 
 Vue.config.productionTip = false;
 
