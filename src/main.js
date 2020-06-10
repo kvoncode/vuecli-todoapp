1041465af888b75058d3e5b7880ac52055dedea6
kvoncode <kvoncode@gmail.com>
2020-06-10 16:13:09 +0900

    pretty: main.js

--- src/main.js
+++ src/main.js
@@ -4,11 +4,11 @@ import App from "./App.vue";
 import "materialize-css/dist/css/materialize.css";
 import "normalize.css";
 
-import store from "./store"
+import store from "./store";
 
 Vue.config.productionTip = false;
 
 new Vue({
   render: (h) => h(App),
-  store
+  store,
 }).$mount("#app");
